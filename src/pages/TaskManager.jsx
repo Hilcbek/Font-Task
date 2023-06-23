bb65d3303033bdc15c5bee1174456fd8f5098540
Hilcbek <balemayehu07@gmail.com>
2023-06-23 16:47:29 +0300

    changes are made 12

--- src/pages/TaskManager.jsx
+++ src/pages/TaskManager.jsx
@@ -23,8 +23,8 @@ export const TaskManager = ({task}) => {
     <ul className='flex items-start justify-start flex-col w-full my-1'>
         <li className='flex p-4 shadow-2xl shadow-zinc-400 bg-white rounded-sm my-1  items-center justify-between w-full'>
            <div className='flex items-start justify-start shadow-lg shadow-zinc-200 w-10/12 p-1 rounded-sm'>
-               <div className='xs:w-8 xs:h-8 flex items-center justify-center'><BsFillPatchCheckFill className='text-green-800 w-full h-full' /></div>
-                <p className={`${task.completed ? 'break-words line-through font-light' : 'line-none'} font-bold ml-2 xs:text-sm sm:text-lg font-Quicksand`}>{task.name}</p>
+               <div className='xs:w-1/12 flex items-center justify-center'><BsFillPatchCheckFill className='text-green-800 w-full h-full' /></div>
+                <p className={`${task.completed ? 'break-words line-through font-light' : 'line-none'} w-11/12 font-bold ml-2 xs:text-sm sm:text-lg font-Quicksand`}>{task.name}</p>
            </div>
             <ul className='flex ml-3 shadow-lg shadow-zinc-300 items-center justify-center p-1 rounded-md xs:w-3/12 lg:w-2/12'>
                 <Link to={`/edit/${task._id}`} className='p-1 rounded-md hover:bg-black/10 cursor-pointer border-solid border-[1px] hover:border-green-800 group'><MdEdit className='group-hover:text-green-800' /></Link>
